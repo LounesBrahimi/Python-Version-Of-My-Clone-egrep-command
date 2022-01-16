a8756e918fc820b24f8e14479ee7c617e94ecb06
LounesBrahimi <lounes.brahimi@etu.sorbonne-universite.fr>
2022-01-16 01:17:16 +0100

    main structure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+#!/usr/bin/python
 
+import sys
 
 #Retourne vraie si l'expression reguliere est reduite à une suite de concatenations
 def estSuiteConcatenations(regEx : str):
@@ -13,9 +15,32 @@ def fileToText(fileName : str):
     f = open(fileName, "r")
     return f.read()
 
+# Methode qui imprime la matrice representant un automate
+
+# Methode qui imprime sur la sortie standard les lignes résutants de la
+# recherche mené avec l'une des deux methodes.
+def printLignes(lignes):
+    for ligne in lignes:
+        print(ligne)
 
 
-#print(estSuiteConcatenations("eB(oo)*k"))
-#print(estSuiteConcatenations("eBook"))
-print(fileToText("ebook1.txt"))
+def main():
+    if (len(sys.argv) < 3):
+        print("Erreur : veuillez introduire l'expression reguliere et le fichier txt")
+    elif((len(sys.argv[1]) == 0) | (len(sys.argv[2]) == 0)):
+        print("Erreur : veuillez introduire l'expression reguliere et le fichier txt")
+    else:
+        print("____________________________________")
+        print("regEx : "+ sys.argv[1])
+        print("____________________________________")
+        if (estSuiteConcatenations(sys.argv[1])):
+            print("=========Recherche avec KMP=========")
+            regEx : str = sys.argv[1]
+            text : str = fileToText(sys.argv[2])
+            # to continue
+        else:
+            print("=========Recherche avec automate=========")
+            regEx : str = sys.argv[1]
+            # to conitnue
 
+main()
